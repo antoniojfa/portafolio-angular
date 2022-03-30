a556cf91bedf47c67ef9747a9ff72ebc5fec1296
Antonio J. Fdez <antonio-jesus.fernandez-amador@t-systems.com>
2022-03-30 12:33:40 +0200

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { InfoEquipo } from '../interfaces/info-equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,29 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
-  constructor( private http: HttpClient) {
-    //Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-        .subscribe( (resp: InfoPagina) => {
-          this.info = resp;
-          this.cargada = true;
-        })
+  equipo: InfoEquipo[] = [];
 
+  constructor( private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo() {
+      //Leer el archivo JSON
+      this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        this.info = resp;
+        this.cargada = true;
+      });
+   }
+
+   private cargarEquipo() {
+        //Leer el archivo JSON
+        this.http.get('https://angular-html-85021-default-rtdb.europe-west1.firebasedatabase.app/equipo.json')
+        .subscribe( resp  => {
+          this.equipo = <InfoEquipo[]>(resp);
+          console.log(resp);
+        });
    }
 }
